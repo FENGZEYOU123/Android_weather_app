d811bfa9d8dbba5194dd27f327e3ded0cb17057a
Franco <fyou@mail.sfsu.edu>
2020-01-14 15:39:14 -0800

    deleted

--- android_weather/app/src/main/java/com/example/andorid_weather/TemperatureConverter.java
+++
@@ -1,8 +0,0 @@
-package com.example.andorid_weather;
-
-public class TemperatureConverter {
-
-    public static float celsiusToFahrenheit(float temperatureInCelsius) {
-        return temperatureInCelsius * 1.8f + 32;
-    }
-}
